efc3a493ade57098c9022d25a1237ffb8da9fbf0
marcosrole <marcosrole@gmail.com>
2016-10-10 11:17:57 -0300

    Arreglando los archivos del servidor

--- app/src/main/java/devazt/networking/ConfigServidor.java
+++ app/src/main/java/devazt/networking/ConfigServidor.java
@@ -4,6 +4,6 @@ package devazt.networking;
  * Created by marco_000 on 10/05/2016.
  */
 public class ConfigServidor {
-    public static String IP ="192.168.1.2";
+    public static String IP ="192.168.0.6";
     public static String URL="app_android";
 }
